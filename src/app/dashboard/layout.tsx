bea9d1bbb740652e1756840b4abed7d91d1ef166
Kushal Harsora <kushalharsora13@gmail.com>
2025-08-04 12:32:36 +0530

    Table and create ticket done

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className=" overflow-hidden">
+      <main className=" flex-1 overflow-y-hidden">
         <SidebarTrigger />
         {children}
       </main>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+// System imports
 import * as React from "react"
 import {
     ColumnDef,
@@ -13,8 +14,26 @@ import {
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table"
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
 
+// Component imports
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import {
@@ -35,37 +54,48 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
 
+// Icon and Style imports
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+
+// Sample Table Data
 const data: Payment[] = [
     {
         id: "m5gr84i9",
         amount: 316,
         status: "success",
-        email: "ken99@example.com",
+        ticket: "ken99@example.com",
+        problem: "Network lag"
     },
     {
         id: "3u1reuv4",
         amount: 242,
         status: "success",
-        email: "Abe45@example.com",
+        ticket: "Abe45@example.com",
+        problem: "Payment lag"
     },
     {
         id: "derv1ws0",
         amount: 837,
         status: "processing",
-        email: "Monserrat44@example.com",
+        ticket: "Monserrat44@example.com",
+        problem: "Firewall issue"
     },
     {
         id: "5kma53ae",
         amount: 874,
         status: "success",
-        email: "Silas22@example.com",
+        ticket: "Silas22@example.com",
+        problem: "Wifi lag"
     },
     {
         id: "bhqecj4p",
         amount: 721,
         status: "failed",
-        email: "carmella@example.com",
+        ticket: "carmella@example.com",
+        problem: "Network lag"
     },
 ]
 
@@ -73,7 +103,8 @@ export type Payment = {
     id: string
     amount: number
     status: "pending" | "processing" | "success" | "failed"
-    email: string
+    ticket: string,
+    problem: string
 }
 
 export const columns: ColumnDef<Payment>[] = [
@@ -107,19 +138,34 @@ export const columns: ColumnDef<Payment>[] = [
         ),
     },
     {
-        accessorKey: "email",
+        accessorKey: "ticket",
         header: ({ column }) => {
             return (
                 <Button
                     variant="ghost"
                     onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
                 >
-                    Email
+                    ticket
                     <ArrowUpDown />
                 </Button>
             )
         },
-        cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+        cell: ({ row }) => <div className="lowercase">{row.getValue("ticket")}</div>,
+    },
+    {
+        accessorKey: "problem",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    problem
+                    <ArrowUpDown />
+                </Button>
+            )
+        },
+        cell: ({ row }) => <div className=" capitalize">{row.getValue("problem")}</div>,
     },
     {
         accessorKey: "amount",
@@ -145,7 +191,7 @@ export const columns: ColumnDef<Payment>[] = [
             return (
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
+                        <Button variant={'ghost'} className="h-8 w-8 p-0">
                             <span className="sr-only">Open menu</span>
                             <MoreHorizontal />
                         </Button>
@@ -196,120 +242,176 @@ export default function Page() {
     })
 
     return (
-        <div className="w-screen h-full p-4 flex flex-col items-center justify-center">
-            <h1>
-                Sample Table
-            </h1>
-            <div className="flex items-center py-4">
-                <Input
-                    placeholder="Filter emails..."
-                    value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-                    onChange={(event) =>
-                        table.getColumn("email")?.setFilterValue(event.target.value)
-                    }
-                    className="max-w-sm"
-                />
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="ml-auto">
-                            Columns <ChevronDown />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        {table
-                            .getAllColumns()
-                            .filter((column) => column.getCanHide())
-                            .map((column) => {
-                                return (
-                                    <DropdownMenuCheckboxItem
-                                        key={column.id}
-                                        className="capitalize"
-                                        checked={column.getIsVisible()}
-                                        onCheckedChange={(value) =>
-                                            column.toggleVisibility(!!value)
-                                        }
-                                    >
-                                        {column.id}
-                                    </DropdownMenuCheckboxItem>
-                                )
-                            })}
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-            <div className="rounded-md border">
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
+        <main className="flex-1 h-full items-center justify-center px-[10vw] max-md:px-[5vw] space-y-4">
+            <div className="w-full h-full p-4 flex flex-col items-center justify-center gap-2">
+                <h1 className=" text-5xl max-lg:text-3xl font-bold">
+                    Ticket Table
+                </h1>
+                <div className="w-full flex justify-evenly items-center gap-2 py-4">
+                    <Input
+                        placeholder="Filter tickets..."
+                        value={(table.getColumn("ticket")?.getFilterValue() as string) ?? ""}
+                        onChange={(event) =>
+                            table.getColumn("ticket")?.setFilterValue(event.target.value)
+                        }
+                        className="max-w-sm"
+                    />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="ml-auto">
+                                Columns <ChevronDown />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            {table
+                                .getAllColumns()
+                                .filter((column) => column.getCanHide())
+                                .map((column) => {
                                     return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext()
-                                                )}
-                                        </TableHead>
+                                        <DropdownMenuCheckboxItem
+                                            key={column.id}
+                                            className="capitalize"
+                                            checked={column.getIsVisible()}
+                                            onCheckedChange={(value) =>
+                                                column.toggleVisibility(!!value)
+                                            }
+                                        >
+                                            {column.id}
+                                        </DropdownMenuCheckboxItem>
                                     )
                                 })}
-                            </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
-                                        </TableCell>
-                                    ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant={'default'}>Create Ticket</Button>
+                        </DialogTrigger>
+                        <DialogContent className="max-sm:max-w-4/5 w-full">
+                            <DialogHeader>
+                                <DialogTitle>Ticket</DialogTitle>
+                                <DialogDescription>
+                                    Enter the ticket details here. Click save when you&apos;re done.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="flex flex-col items-start justify-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Ticket Title
+                                    </Label>
+                                    <Input id="name" className="col-span-3" />
+                                </div>
+                                <div className="flex flex-col items-start justify-center gap-4">
+                                    <Label htmlFor="username" className="text-left">
+                                        Description
+                                    </Label>
+                                    <Textarea id="username" className="col-span-3 min-h-[100px] max-h-[800px] overflow-y-auto" />
+                                </div>
+                                <div className="flex flex-col items-start justify-center gap-4">
+                                    <Label htmlFor="username" className="text-left">
+                                        Attack Image
+                                    </Label>
+                                    <Input id="picture" type="file" />
+                                </div>
+                                <div className="flex flex-col items-start justify-center gap-4">
+                                    <Label htmlFor="username" className="text-left">
+                                        Priority
+                                    </Label>
+                                    <Select>
+                                        <SelectTrigger className="w-full">
+                                            <SelectValue placeholder="Select ticket priority" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>Ticket Priority</SelectLabel>
+                                                <SelectItem value="high">High</SelectItem>
+                                                <SelectItem value="medium">Medium</SelectItem>
+                                                <SelectItem value="low">Low</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <Button type="submit">Save changes</Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+                <div className="w-full rounded-md border">
+                    <Table>
+                        <TableHeader>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => {
+                                        return (
+                                            <TableHead key={header.id}>
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                            </TableHead>
+                                        )
+                                    })}
                                 </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className="h-24 text-center"
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
-            <div className="flex items-center justify-end space-x-2 py-4">
-                <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                            ))}
+                        </TableHeader>
+                        <TableBody>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={row.getIsSelected() && "selected"}
+                                    >
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext()
+                                                )}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell
+                                        colSpan={columns.length}
+                                        className="h-24 text-center"
+                                    >
+                                        No results.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
                 </div>
-                <div className="space-x-2">
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        onClick={() => table.previousPage()}
-                        disabled={!table.getCanPreviousPage()}
-                    >
-                        Previous
-                    </Button>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        onClick={() => table.nextPage()}
-                        disabled={!table.getCanNextPage()}
-                    >
-                        Next
-                    </Button>
+                <div className="flex items-center justify-end space-x-2 py-4">
+                    <div className="flex-1 text-sm text-muted-foreground">
+                        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                        {table.getFilteredRowModel().rows.length} row(s) selected.
+                    </div>
+                    <div className="space-x-2">
+                        <Button
+                            variant="outline"
+                            size="sm"
+                            onClick={() => table.previousPage()}
+                            disabled={!table.getCanPreviousPage()}
+                        >
+                            Previous
+                        </Button>
+                        <Button
+                            variant="outline"
+                            size="sm"
+                            onClick={() => table.nextPage()}
+                            disabled={!table.getCanNextPage()}
+                        >
+                            Next
+                        </Button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
