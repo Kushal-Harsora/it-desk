3c4fc7462bd8223a64567f13835c784dd423b09f
Kushal Harsora <kushalharsora13@gmail.com>
2025-08-06 14:59:22 +0530

    Updated Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,25 +3,37 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
-  const isPrivatePath = path.startsWith("/dashboard") || path.startsWith("/admin/dashboard");
-  const isLoginPath = path === "/login";
+  const isPrivatePathPublic = path.startsWith("/dashboard")
+  const isPrivatePathAdmin = path.startsWith("/admin/dashboard");
+  const isLoginPath: boolean = path === "/login";
+  const isLoginPathAdmin: boolean = path === '/admin';
 
   const token = request.cookies.get("token")?.value || "";
 
   // 1. If user is trying to access login page while already logged in → redirect to "/"
-  if (isLoginPath && token) {
-    const response = NextResponse.redirect(new URL("/dashboard", request.url));
-    // Clear the token cookie (optional, based on your logic)
-    response.cookies.set("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    });
-    return response;
+  if (token) {
+    if (isLoginPath) {
+      const response = NextResponse.redirect(new URL("/login", request.url));
+      response.cookies.set("token", "", {
+        httpOnly: true,
+        expires: new Date(0),
+      });
+      return response;
+    } else if (isLoginPathAdmin) {
+      const response = NextResponse.redirect(new URL("/admin", request.url));
+      response.cookies.set("token", "", {
+        httpOnly: true,
+        expires: new Date(0),
+      });
+      return response;
+    }
   }
 
   // 2. If user is accessing a private path without token → redirect to login
-  if (isPrivatePath && !token) {
+  if (isPrivatePathPublic && !token) {
     return NextResponse.redirect(new URL("/login", request.url));
+  } else if (isPrivatePathAdmin && !token) {
+    return NextResponse.redirect(new URL("/admin", request.url));
   }
 
   // 3. Default allow
@@ -34,8 +46,10 @@ export function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
+    '/admin/:path*',
     '/dashboard/:path*',
     '/api/:path*',
-    '/login'
+    '/login',
+    '/admin'
   ],
 }
